153381cb3b01ac31aa97a3b1e30b3d96eebb8542
Aborio <aguborio18@gmail.com>
2023-04-14 18:05:26 -0300

    entrega rick and morty

---
+++ src/components/Detail.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+
+
+const Detail = () => {
+    const [character, setCharacter] = useState({});
+    const { id } = useParams();
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+        .then(response => {
+            setCharacter(response.data);
+        })
+    }, [id]);
+    return (
+        <div>
+            <h1 className="bcard">{character.name}</h1>
+            <p className="bcard"> {character.status}</p>
+            <p className="bcard">{character.species}</p>
+            <p className="bcard"> {character.gender}</p>
+            <img src={character.image} alt={character.name} />
+        </div>
+    );
+}
+export default Detail;
